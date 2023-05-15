94f2f9ad1d0b73a03d9bb7548566a9558b19dd27
Core|Threat <30553333+ipcis@users.noreply.github.com>
2023-05-15 10:42:52 +0200

    Create ioc_check_v4.py

---
+++ ioc_check_v4.py
@@ -0,0 +1,57 @@
+import csv
+import requests
+import socket
+import ipaddress
+
+def is_valid_ip(ip_address):
+    try:
+        socket.inet_aton(ip_address)
+        return True
+    except socket.error:
+        return False
+
+def is_private_ip(ip_address):
+    try:
+        ip = ipaddress.ip_address(ip_address)
+        return ip.is_private
+    except ValueError:
+        return False
+
+def load_exit_nodes():
+    url = "https://check.torproject.org/exit-addresses"
+    response = requests.get(url)
+    exit_nodes = []
+
+    lines = response.text.split('\n')
+    for line in lines:
+        if line.startswith('ExitAddress'):
+            parts = line.split(' ')
+            if len(parts) >= 2:
+                exit_nodes.append(parts[1])
+
+    return exit_nodes
+
+def is_tor_exit_node(ip_address, exit_nodes):
+    return ip_address in exit_nodes
+
+def check_tor_exit_nodes(csv_file):
+    exit_nodes = load_exit_nodes()
+
+    with open(csv_file, 'r') as file:
+        reader = csv.reader(file)
+        ip_addresses = [row[0] for row in reader]
+
+    for ip_address in ip_addresses:
+        if is_valid_ip(ip_address):
+            if not is_private_ip(ip_address):
+                if is_tor_exit_node(ip_address, exit_nodes):
+                    print(f"{ip_address} is a Tor exit node relay.")
+                else:
+                    print(f"{ip_address} is not a Tor exit node relay.")
+            else:
+                print(f"{ip_address} is a private IP address and will not be checked.")
+        else:
+            print(f"{ip_address} is not a valid IP address.")
+
+# Beispielaufruf
+check_tor_exit_nodes('ip_addresses.csv')
